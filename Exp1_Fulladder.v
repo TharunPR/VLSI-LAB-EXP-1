e57b4d4cf033b2f8a55f7f1d9ed08ad2bcde0660
Tharun <117915125+TharunPR@users.noreply.github.com>
2024-04-02 23:05:53 +0530

    Update Exp1_Fulladder.v

--- Exp1_Fulladder.v
+++ Exp1_Fulladder.v
@@ -1,8 +1,10 @@
-module fa_ha(a,b,c,sum,carry);
+module fulladder(a,b,c,sum,carry);
 input a,b,c;
 output sum,carry;
-
-
-
-
+wire w1,w2,w3;
+xor(w1,a,b);
+xor(sum,w1,c);
+and(w2,w1,c);
+and(w3,a,b);
+or(carry,w2,w3);
 endmodule
